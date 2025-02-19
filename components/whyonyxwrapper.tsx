96304cbaf177f04c1f487e8b54ae865e27dc5493
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-19 17:43:30 -0500

    Create whyonyxwrapper.tsx

---
+++ components/whyonyxwrapper.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import type React from "react"
+
+import WhyOnyx from "./whyonyx"
+
+const WhyOnyxWrapper: React.FC = () => {
+  return <WhyResendIt />
+}
+
+export default WhyOnyxWrapper
