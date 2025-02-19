831c5fb9e4429deecb8dfce793f273391f4c91c3
Robert Mourey Jr <rmourey_jr@quantumone.io>
2025-02-19 17:53:54 -0500

    Update whyonyxwrapper.tsx

--- components/whyonyxwrapper.tsx
+++ components/whyonyxwrapper.tsx
@@ -5,7 +5,7 @@ import type React from "react"
 import WhyOnyx from "./whyonyx"
 
 const WhyOnyxWrapper: React.FC = () => {
-  return <WhyResendIt />
+  return <WhyOnyx />
 }
 
 export default WhyOnyxWrapper
